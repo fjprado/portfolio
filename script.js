7ce5d27f12987ae8fd8742e555081190f4273b2a
FernandoPrado <fernando.prado@bp.com>
2023-09-14 00:57:27 -0700

    Final version before projects and dynamic values

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function toggleMenu(){
-    const menu = document.querySelector(".mobile__menu_links");
-    const icon = document.querySelector(".mobile__menu_icon");
+    const menu = document.querySelector(".mobile__menu-links");
+    const icon = document.querySelector(".mobile__menu-icon");
+
+    const barProgress = document.querySelector(".bar");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
 }
